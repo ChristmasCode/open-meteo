ad75547ce14ad3668c8ac6548011ecd04a9baf25
ChristmasCode <alexandr.gotchenya@gmail.com>
2024-07-19 19:16:48 +0300

    refactoring

--- weather_app/get_weather/tests.py
+++ weather_app/get_weather/tests.py
@@ -1,16 +1,3 @@
 from django.test import TestCase
-from django.urls import reverse
 
-
-class IndexTestCase(TestCase):
-    def test_index_view_success(self):
-        response = self.client.post(reverse('index'), {'city': 'New York'})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
-        self.assertIn('current_temperature', response.context)
-        self.assertIn('current_apparent_temperature', response.context)
-
-    def test_index_view_get_request(self):
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+# Create your tests here.
