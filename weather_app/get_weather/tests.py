2c087bb395ee10b479770590fd2369da06b87b5d
ChristmasCode <alexandr.gotchenya@gmail.com>
2024-07-19 14:38:07 +0300

    add IndexTestCase

--- weather_app/get_weather/tests.py
+++ weather_app/get_weather/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexTestCase(TestCase):
+    def test_index_view_success(self):
+        response = self.client.post(reverse('index'), {'city': 'New York'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertIn('current_temperature', response.context)
+        self.assertIn('current_apparent_temperature', response.context)
+
+    def test_index_view_get_request(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
