ee2053fe7adb5114add6612e9b3fc691aae85d0f
ChristmasCode <alexandr.gotchenya@gmail.com>
2024-07-19 17:27:48 +0300

    edit get_city_coordinates

--- weather_app/get_weather/utils.py
+++ weather_app/get_weather/utils.py
@@ -1,19 +1,22 @@
 import requests
 from loguru import logger
 
-
 def get_city_coordinates(city_name):
     url = f"https://nominatim.openstreetmap.org/search?q={city_name}&format=json"
     try:
         response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
         data = response.json()
 
         if data:
-            latitude = float(data[0]['lat'])
-            longitude = float(data[0]['lon'])
-            return {'latitude': latitude, 'longitude': longitude}
+            if data[0].get('lat') and data[0].get('lon'):
+                latitude = float(data[0]['lat'])
+                longitude = float(data[0]['lon'])
+                return {'latitude': latitude, 'longitude': longitude}
+            else:
+                raise ValueError("No valid city data found")
         else:
             raise ValueError("No city data found")
-    except requests.exceptions.RequestException as e:
+    except (requests.exceptions.RequestException, ValueError) as e:
         logger.error(f"Error getting city coordinates: {e}")
         raise Exception("Failed to get city coordinates")
